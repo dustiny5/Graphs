4ed1d05524535eb94de3c39835d9873dbd865fd4
dustiny5 <dustiny5@users.noreply.github.com>
2019-11-06 14:14:48 -0800

    Update notes

--- projects/social/social.py
+++ projects/social/social.py
@@ -13,19 +13,6 @@ class Queue():
     def size(self):
         return len(self.queue)
 
-class Stack():
-    def __init__(self):
-        self.stack = []
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-    def size(self):
-        return len(self.stack)
-
 class User:
     def __init__(self, name):
         self.name = name
@@ -156,5 +143,6 @@ if __name__ == '__main__':
     print(sg.users)
     print("FRIENDSHIPS:")
     print(sg.friendships)
+    print("CONNECTIONS")
     connections = sg.getAllSocialPaths(1)
     print(connections)
